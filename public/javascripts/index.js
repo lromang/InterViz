ccc781dd2d81cb1a4e2210db1d156cbc1b7ef259
lromang <luis.roangarci@gmail.com>
2017-12-27 09:08:06 -0600

    initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+/*
+ * Function to execute on document ready
+ */
+$(document).ready(function(){
+    d3.select('body')
+      .append('svg')
+      .attr('height', '100px')
+      .attr('width', '100px')
+})
